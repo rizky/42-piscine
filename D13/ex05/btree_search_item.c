f51a8c8bb58478d0aa6d8215d879f04ace9113c3
Rizky ario NUGROHO <rnugroho@e1r2p8.42.fr>
2017-08-22 17:30:28 +0200

    Cleaned up D13

--- D13/ex05/btree_search_item.c
+++ D13/ex05/btree_search_item.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_search_item.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnugroho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/22 17:13:11 by rnugroho          #+#    #+#             */
+/*   Updated: 2017/08/22 17:13:12 by rnugroho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_btree.h"
 
-void *btree_search_item(t_btree *root,
+void	*btree_search_item(t_btree *root,
 	void *data_ref, int (*cmpf)(void *, void *))
 {
 	if (!(root))
